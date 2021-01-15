50cb975382234660fa273293aaac3d926ffddb5f
橋本侑大 <hasi0620yuta02fact@gmail.com>
2021-01-15 22:15:04 +0900

    [Add]HOME画面、ログイン画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ユーザーページ
+  namespace :public, path: '' do
+    root to: 'homes#top'
+    get 'about', to: 'homes#about'
+  end
+  
+  # 管理者ページ
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
+
+  #devise設定
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations',
+  }
+
+  devise_for :admins, path: 'admin', controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations',
+  }
 end
